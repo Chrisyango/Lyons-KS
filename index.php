b03258b1301ffacdfa27cad7330d27d0cffff443
Chris Yang <cyang287@yahoo.com>
2019-03-28 16:38:10 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>City of Lyons, KS</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/css/animate.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header class="clearfix">
+	<a href="#main" id="skip" tabindex="0">SKIP TO CONTENT</a>
+
+	<div class="container">
+		<a href="./" id="logo">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div tabindex="0" id="search-toggle-mobile" class="fa fa-search"></div>
+			<div tabindex="0" id="nav-toggle" class="fa fa-bars"></div>	
+		</div><!-- /#toggles -->
+
+		<nav>
+			<ul id="nav">
+				<li>
+					<a href="./">departments</a>
+					<ul>
+						<li>
+							<a href="./">
+								departments
+								<span>need help?</span>
+							</a>
+						</li>
+						<li><a href="./">administration</a></li>
+						<li><a href="./">police department</a></li>
+						<li><a href="./">composting</a></li>
+						<li><a href="./">parks <span>+</span> grounds</a></li>
+						<li><a href="./">airport</a></li>
+						<li><a href="./">cemetery department</a></li>
+						<li><a href="./">fire department</a></li>
+						<li><a href="./">street <span>+</span> food control</a></li>
+						<li><a href="./">municipal courts</a></li>
+						<li><a href="./">utilities department</a></li>
+					</ul>
+				</li>
+				<li><a href="./">government</a></li>
+				<li><a href="./">residents</a></li>
+				<li><a href="./">visitors</a></li>
+				<li><a href="./">business</a></li>
+				<li><a href="./">how do i...</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+
+		<div id="google-translate">
+			<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+			<div id="google_translate_element"></div>
+			<script type="text/javascript">
+				function googleTranslateElementInit() {
+					new google.translate.TranslateElement({  
+						pageLanguage: 'en', 
+						layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+					}, 'google_translate_element');
+				}
+			</script>
+			<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+		</div><!--/#google-translate.control.hidden-sm.hidden-xs-->
+
+		<div class="weather hidden-sm hidden-xs">
+			<div id="weather"></div><!-- /#weather -->
+		</div><!-- /.weather -->	
+	</div><!-- /.container -->
+</header><!-- /.clearfix -->
+
+<section id="slider">
+	<ul class="bxslider">
+		<li style="background: url('./_assets_/images/hero.jpg') center no-repeat;background-size:cover;"></li>
+	</ul><!-- /.bxslider -->
+
+	<div class="container">
+		<h1><span>welcome to</span>The City of Lyons</h1>
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="Enter search terms..." type="search" id="search-input">
+				<button><i class="fa fa-search"></i></button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+
+		<a href="./" class="btn">take a tour</a>
+
+		<div class="social-links hidden-sm hidden-xs">
+			<a href="./" class="fa fa-facebook"></a>
+			<a href="./" class="fa fa-youtube"></a>
+		</div><!-- /.social-links -->
+	</div><!-- /.container -->
+</section><!-- /#slider -->
+
+<main id="main">
+	<section id="quick-links">
+		<div class="container">
+			<div id="quick-links-wrapper" class="owl-carousel">
+				<a href="./" class="quick-link">
+					<div class="quick-link-img" style="background:url('./_assets_/images/ql1.png') center no-repeat;background-size:cover;"></div>
+					<span>online bill pay</span>
+				</a><!-- /.quick-link -->
+				<a href="./" class="quick-link">
+					<div class="quick-link-img" style="background:url('./_assets_/images/ql2.png') center no-repeat;background-size:cover;"></div>
+					<span>document center</span>
+				</a><!-- /.quick-link -->
+				<a href="./" class="quick-link">
+					<div class="quick-link-img" style="background:url('./_assets_/images/ql3.png') center no-repeat;background-size:cover;"></div>
+					<span>resources</span>
+				</a><!-- /.quick-link -->
+			</div><!-- /#quick-links-wrapper.owl-carousel -->
+		</div><!-- /.container -->
+	</section><!-- /#quick-links -->
+
+	<section id="content">
+		<section id="news">
+			<div id="news-bg" style="background:url('./_assets_/images/news-bg.png') center no-repeat;background-size:cover;"></div>
+			<div class="container">
+				<div class="clearfix">
+					<h2>news stories <span>+</span> headlines</h2>
+					<a href="./" class="btn">archives</a>
+				</div><!-- /.clearfix -->
+				<div id="news-links" class="owl-carousel">
+					<a href="./" class="news-link">
+						<div class="news-link-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+						<div class="news-link-info equal">
+							<h3>news post title cras egestas ligula purus</h3>
+							<span>posted january 28, 2019</span>
+						</div><!-- /.news-link-info -->
+					</a><!-- /.news-link -->
+					<a href="./" class="news-link">
+						<div class="news-link-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+						<div class="news-link-info equal">
+							<h3>news post title cras egestas ligula purus</h3>
+							<span>posted january 28, 2019</span>
+						</div><!-- /.news-link-info -->
+					</a><!-- /.news-link -->
+					<a href="./" class="news-link">
+						<div class="news-link-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+						<div class="news-link-info equal">
+							<h3>news post title cras egestas ligula purus</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.</p>
+							<span>posted january 28, 2019</span>
+						</div><!-- /.news-link-info -->
+					</a><!-- /.news-link -->
+				</div><!-- /#news-links -->
+			</div><!-- /.container -->
+		</section><!-- /#news -->
+
+		<section id="content-quick-links">
+			<div class="container">
+				<div id="content-quick-links-wrapper" class="owl-carousel">
+					<a href="./" class="content-quick-link">
+						<div class="content-quick-link-img" style="background:url('./_assets_/images/cql1.png') center no-repeat;background-size:cover;"></div>
+						<span>Forms</span>
+					</a><!-- /.content-quick-link -->
+					<a href="./" class="content-quick-link">
+						<div class="content-quick-link-img" style="background:url('./_assets_/images/cql2.png') center no-repeat;background-size:cover;"></div>
+						<span>Maps/GIS</span>
+					</a><!-- /.content-quick-link -->
+					<a href="./" class="content-quick-link">
+						<div class="content-quick-link-img" style="background:url('./_assets_/images/cql3.png') center no-repeat;background-size:cover;"></div>
+						<span>Report an Issue</span>
+					</a><!-- /.content-quick-link -->
+					<a href="./" class="content-quick-link">
+						<div class="content-quick-link-img" style="background:url('./_assets_/images/cql4.png') center no-repeat;background-size:cover;"></div>
+						<span>Agendas/Minutes</span>
+					</a><!-- /.content-quick-link -->
+					<a href="./" class="content-quick-link">
+						<div class="content-quick-link-img" style="background:url('./_assets_/images/cql5.png') center no-repeat;background-size:cover;"></div>
+						<span>Emergency Info</span>
+					</a><!-- /.content-quick-link -->
+				</div><!-- /#content-quick-links-wrapper -->
+			</div><!-- /.container -->
+		</section><!-- /#content-quick-links -->
+
+		<section id="events">
+			<div class="container">
+				<div class="clearfix">
+					<h2>upcoming events</h2>
+					<ul id="tabs">
+						<li><a href="./" class="btn">events</a></li>
+						<li><a href="./" class="btn">meetings</a></li>
+					</ul><!-- /#tabs -->
+					<li><a href="./" class="btn">go to calendar</a></li>
+				</div>
+				<div id="tabs-content">
+					<div class="current">
+						<img src="./_assets_/images/events.png" alt="Events">
+					</div>
+					<div>
+						MEETINGS YO!
+					</div>
+				</div><!-- /#tabs-content -->
+			</div><!-- /.container -->
+		</section><!-- /#events -->
+
+		<section id="alerts">
+			<div class="container">
+				<form>
+					<label for="signup-input">alerts <span>+</span> notifications</label>
+					<div>
+						<input name="q" class="form-control" placeholder="Enter your email address" id="signup-input">
+						<button class="btn">submit</button>
+					</div>
+				</form>
+			</div><!-- /.container -->
+		</section><!-- /#alerts -->
+	</section><!-- /#content -->
+</main>
+
+<footer>
+	<div class="container">
+		<section id="top-footer">
+			<section id="contact-info">
+				<span><span>&copy; 2019</span> City of Lyons, Kansas</span>
+				<span><span>P:</span> 734-426-8303</span>
+				<span><span>F:</span> 734-426-5614</span>
+				<div>201 W Main Street, PO Box 808 - Lyons, KS 67554</div>
+			</section><!-- /#contact-info -->
+
+			<div class="social-links hidden-sm hidden-xs">
+				<a href="./" class="fa fa-facebook"></a>
+				<a href="./" class="fa fa-youtube"></a>
+			</div><!-- /.social-links -->
+		</section><!-- /#top-footer.clearfix -->
+		
+		<section id="revize-logo">
+			<span>Powered by <a href="https://www.revize.com/" id="revize-logo">revize</a>, the Government Website Experts</span>
+			<div id="footer-links">
+				<a href="./">Contact Us</a>
+				<a href="./">Accessibility</a>
+			</div>
+		</section><!-- /#revize-logo -->
+	</div><!-- /.container -->
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
